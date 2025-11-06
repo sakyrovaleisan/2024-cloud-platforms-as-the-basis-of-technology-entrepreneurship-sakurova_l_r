360a3a7fd004f420ec239d7253075cab065dd870
Leisan <sakyrovaleisan@gmail.com>
2025-11-07 01:55:10 +0300

    Update bot.py

--- project/bot.py
+++ project/bot.py
@@ -1,14 +1,6 @@
-from telegram import Update
-from telegram.ext import CommandHandler, ApplicationBuilder, ContextTypes
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv()  # загружает переменные из .env
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-async def whoami(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f"Your chat_id: {update.effective_chat.id}")
-
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("whoami", whoami))
-app.run_polling()
+print("BOT_TOKEN =", BOT_TOKEN) 
