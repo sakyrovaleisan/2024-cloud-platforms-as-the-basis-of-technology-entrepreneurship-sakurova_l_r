02835836a02faff5894aa6b22d07e186ed2e77a6
Leisan <sakyrovaleisan@gmail.com>
2025-11-07 01:42:07 +0300

    Update bot.py

--- project/bot.py
+++ project/bot.py
@@ -1,9 +1,14 @@
 from telegram import Update
 from telegram.ext import CommandHandler, ApplicationBuilder, ContextTypes
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def whoami(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"Your chat_id: {update.effective_chat.id}")
 
-app = ApplicationBuilder().token("ВАШ_BOT_TOKEN").build()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("whoami", whoami))
 app.run_polling()
